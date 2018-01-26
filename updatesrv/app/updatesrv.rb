303321f879753f22d7cab8e25889d928904d8764
R. Tyler Croy <tyler@monkeypox.org>
2018-01-26 05:01:56 -0800

    Prorotyping SSE streaming in Ruby

--- updatesrv/app/updatesrv.rb
+++ updatesrv/app/updatesrv.rb
@@ -1,3 +1,5 @@
+require 'concurrent/array'
+require 'concurrent/hash'
 require 'hashie'
 require 'sinatra/base'
 require 'sinatra/json'
@@ -7,6 +9,8 @@ require 'apps/jenkins'
 
 module Updatesrv
   class App < Sinatra::Base
+    set :connections => Concurrent::Array.new
+
     before do
       if Thread.current[:apps].nil?
         Thread.current[:apps] = {
@@ -17,6 +21,12 @@ module Updatesrv
     end
 
     get '/' do
+      settings.connections.each do |conn|
+        m = "data: #{Time.now.utc}\n\n"
+        puts m
+        conn << m
+      end
+
       'Evergreen Update Service'
     end
 
@@ -27,6 +37,17 @@ module Updatesrv
         response[:apps][name] = app.last_refreshed?
       end
       json response
+    end
+
+    get '/sse', :provides => 'text/event-stream' do
+      stream(:keep_open) do |conn|
+        puts "Received conn: #{conn}"
+        settings.connections.reject! do |c|
+          puts "rejecting #{c}" if c.closed?
+          c.closed?
+        end
+        settings.connections << conn
+      end
     end
 
     post '/check/:app' do |app|
