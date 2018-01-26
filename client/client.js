046ed2bbc54801425b6d7b10449e4b5ba946a2d8
R. Tyler Croy <tyler@monkeypox.org>
2018-01-26 07:36:51 -0800

    Support querying and restarting processes in the container via evergreen-client

--- client/client.js
+++ client/client.js
@@ -1,9 +1,7 @@
 //!/usr/bin/env node
 
 const EventSource = require('eventsource');
-
-console.log("Hello");
-
+const supervisor  = require('./lib/supervisor.js')
 
 let stream = new EventSource('http://127.0.0.1:9292/sse');
 
@@ -11,6 +9,14 @@ stream.onmessage = (ev) => {
   console.log(ev);
 };
 
+supervisor.isRunning().then((running) => {
+    if (!running) { return; }
+    console.log('Supervisord can be accessed..');
+
+    supervisor.restartProcess('jenkins').then(() => {
+        console.log('restarted....');
+    });
+});
 
 module.exports = () => {
 };
