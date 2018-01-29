372e562c30b4ed517a8878d069c5a2c7f06fd64e
R. Tyler Croy <tyler@monkeypox.org>
2018-01-29 01:02:09 -0800

    Add some simple logging to the client

--- client/client.js
+++ client/client.js
@@ -1,26 +1,44 @@
 //!/usr/bin/env node
 
 const EventSource = require('eventsource');
+const util        = require('util');
 const supervisor  = require('./lib/supervisor.js')
 
-let s = new EventSource('http://127.0.0.1:9292/sse');
+const ENDPOINT    = process.env.EVERGREEN_ENDPOINT;
+console.debug('Using the Evergreen endpoint:', ENDPOINT);
 
-s.addEventListener('restart', (ev) => {
-  console.log(ev);
-    supervisor.isRunning().then((running) => {
-        if (!running) { return; }
-        console.log('Supervisord can be accessed..');
+const sse = new EventSource(util.format('%s/sse', ENDPOINT));
+console.debug('EventSource created', sse);
 
-        supervisor.restartProcess('jenkins').then(() => {
-            console.log('restarted....');
-        });
-    });
-
-}, false);
-s.onmessage = (ev) => {
-  console.log(ev);
+/* First set up a generic event handler.
+ *
+ * This function will be called whenever an unnamed SSE is received, an that
+ * should typically never happen.
+ */
+sse.onmessage = function(ev) {
+  console.error('Unhandled message from Evergreen:', ev);
 };
-console.log(s);
 
+
+/* Types of Commands we can process */
+['ping', 'update', 'flags', 'logs'].map((command) => {
+    console.debug('Adding SSE event listener for', command);
+});
+
+sse.addEventListener('restart', (ev) => {
+  console.log(ev);
+  return;
+
+  supervisor.isRunning().then((running) => {
+      if (!running) { return; }
+      console.log('Supervisord can be accessed..');
+
+      supervisor.restartProcess('jenkins').then(() => {
+          console.log('restarted....');
+      });
+  });
+}, false);
+
+/* Currently not exporting any symbols */
 module.exports = () => {
 };
