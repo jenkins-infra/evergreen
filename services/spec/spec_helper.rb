69356f953d0b19b136886306c58bf2fde25ec6d7
R. Tyler Croy <tyler@monkeypox.org>
2018-02-06 10:05:18 -0800

    Set some necessary environments in a test context

--- services/spec/spec_helper.rb
+++ services/spec/spec_helper.rb
@@ -2,6 +2,9 @@ require 'rspec'
 
 $LOAD_PATH << File.expand_path(File.dirname(__FILE__) + '/../')
 
+ENV['RACK_ENV'] = 'test'
+ENV['ASSETS_ENDPOINT'] = '/assets'
+
 RSpec.configure do |c|
   c.order = :random
 end
