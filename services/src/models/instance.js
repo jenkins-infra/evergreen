4c09730ec1c28c0d33029721ce5e1dda69b2beee
R. Tyler Croy <tyler@monkeypox.org>
2018-03-21 09:40:07 -0700

    Ensure we're referring to the right `instances` table already in the DB

--- services/src/models/instance.js
+++ services/src/models/instance.js
@@ -4,7 +4,7 @@ const DataTypes = Sequelize.DataTypes;
 
 module.exports = function (app) {
   const sequelizeClient = app.get('sequelizeClient');
-  const instance = sequelizeClient.define('instance', {
+  const instance = sequelizeClient.define('instances', {
     uuid: DataTypes.UUID,
     timezone: DataTypes.STRING,
     channelId: DataTypes.BIGINT,
