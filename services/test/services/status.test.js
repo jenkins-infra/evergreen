fe74734e3152ac595085d8952390c863c217016c
R. Tyler Croy <tyler@monkeypox.org>
2018-03-20 17:06:28 -0700

    Attempting to hack transaction support into the integration tests
    This isn't working properly, so I'm going to junk it, but I wanted it in the
    history in case it proved useful in the future

--- services/test/services/status.test.js
+++ services/test/services/status.test.js
@@ -1,10 +1,58 @@
+/*
+ * This file contains integration tests for the 'status' service which is
+ * primarily involved in managing `Instance` data
+ */
+
 const assert = require('assert');
+const uuid = require('uuid/v4');
+// eslint-disable-next-line no-unused-vars
+const logger = require('winston');
+const cls = require('continuation-local-storage');
+const namespace = cls.createNamespace('testing');
+const sequelize = require('sequelize');
+sequelize.useCLS(namespace);
+
 const app = require('../../src/app');
 
 describe('\'status\' service', () => {
+  before(() => {
+    app.hooks({
+      before(hook) {
+        return new Promise(resolve => {
+          let sequelize = app.get('sequelizeClient');
+          sequelize.transaction().then(trx => {
+            hook.params.transaction = trx;
+            resolve(hook);
+          });
+        });
+      },
+      after(hook) {
+        hook.params.transaction.rollback().then(() => hook);
+      }
+    });
+  });
+
   it('registered the service', () => {
     const service = app.service('status');
 
     assert.ok(service, 'Registered the service');
   });
+
+  it('has no status by default', async () => {
+    const items = await app.service('status').find();
+    assert.equal(items.total, 0, 'Status records were found');
+  });
+
+  describe('creating Status', () => {
+    it('should allow a new Status to be created', async () => {
+      const service = app.service('status');
+      const response = await service.create({
+        uuid: uuid()
+      }, {});
+
+      assert.ok(response, 'Response looks acceptable');
+
+    });
+  });
+
 });
