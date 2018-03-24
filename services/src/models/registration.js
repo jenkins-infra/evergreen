c15cacb820a7cadf33f60cce793d81faf8cec13d
R. Tyler Croy <tyler@monkeypox.org>
2018-03-23 17:05:32 -0700

    Add a first version of a registration model

---
+++ services/src/models/registration.js
@@ -0,0 +1,20 @@
+'use strict';
+const Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes;
+
+module.exports = function (app) {
+  const sequelizeClient = app.get('sequelizeClient');
+  const reg = sequelizeClient.define('registration', {
+    uuid: DataTypes.UUID,
+    pubKey: DataTypes.STRING,
+    createdAt: DataTypes.DATE
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  reg.associate = function (models) {
+    // Define associations here
+    // See http://docs.sequelizejs.com/en/latest/docs/associations/
+  };
+
+  return reg;
+};
