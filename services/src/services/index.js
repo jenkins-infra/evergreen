4fe7352d624479056d2d745b03908a5862f1e35e
R. Tyler Croy <tyler@monkeypox.org>
2018-04-16 13:27:45 -0700

    Generate a stub for the update service
    Need to define these interactions ab it more in a JEP to clarify my thinking

--- services/src/services/index.js
+++ services/src/services/index.js
@@ -5,6 +5,8 @@ const registration = require('./registration/registration.service');
 
 const versions = require('./versions/versions.service.js');
 
+const update = require('./update/update.service.js');
+
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(auth);
@@ -12,4 +14,5 @@ module.exports = function (app) {
   app.configure(pusher);
   app.configure(registration);
   app.configure(versions);
+  app.configure(update);
 };
