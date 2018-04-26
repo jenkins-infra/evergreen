32be4add37273f424e48d67652c96c8ce3273d7f
R. Tyler Croy <tyler@monkeypox.org>
2018-04-26 16:22:32 -0700

    Refactor the status hooks to be a little bit cleaner
    module.exports hacks are no bueno

--- services/src/services/status/status.hooks.js
+++ services/src/services/status/status.hooks.js
@@ -7,110 +7,93 @@ const internalOnly       = require('../../hooks/internalonly');
 const ensureMatchingUUID = require('../../hooks/ensureuuid');
 
 /*
+ * StatusHooks are all the hooks necessary to run the status service properly
+ */
+class StatusHooks {
+  constructor () {
+  }
+
+  getHooks() {
+    return {
+      before: {
+        all: [
+          authentication.hooks.authenticate(['jwt'])
+        ],
+        get: [
+          this.includeAssociations,
+        ],
+        create: [
+          ensureMatchingUUID,
+          module.exports.defaultUpdateLevel,
+          module.exports.pruneQueryParams,
+        ],
+
+        update: [
+          ensureMatchingUUID,
+        ],
+        patch: [
+          ensureMatchingUUID,
+        ],
+        remove: [
+          internalOnly
+        ]
+      },
+      after: {},
+      error: {}
+    };
+  }
+
+  /*
+   * Include the model's associations in the output from the hook
+   */
+  includeAssociations(context) {
+    if (!context.params.sequelize) {
+      context.params.sequelize = {};
+    }
+    Object.assign(context.params.sequelize, {
+      include: [ context.app.get('models').update ]
+    });
+    return context;
+  }
+
+  /*
+   * delete extra parameters included in the query string
+   */
+  pruneQueryParams(context) {
+    if (context.params.query) {
+      delete context.params.query.include;
+    }
+    return context;
+  }
+
+  /*
+   * Default new instances into the latest update record in the `general` channel.
+   */
+  async defaultUpdateLevel(context) {
+    const updates = context.app.service('update');
+    const result = await updates.find({
+      query: {
+        tainted: false,
+        channel: 'general',
+        $limit: 1,
+        $sort: {
+          createdAt: -1,
+        }
+      },
+    });
+
+    if (result.total == 0) {
+      throw new Error('Failed to find the latest `general` updates for instance creation');
+    }
+    /*
+      * The result returned is a paginated object
+      */
+    context.data.updateId = result.data[0].id;
+    return context;
+  }
+}
+
+/*
  * To make things easier to unit test, these hook functions are being exported
  */
-module.exports = {};
-
-/*
- * Include the model's associations in the output from the hook
- */
-module.exports.includeAssociations = function(context) {
-  if (!context.params.sequelize) {
-    context.params.sequelize = {};
-  }
-  Object.assign(context.params.sequelize, {
-    include: [ context.app.get('models').update ]
-  });
-  return context;
-};
-
-/*
-  * delete extra parameters included in the query string
-  */
-module.exports.pruneQueryParams = function(context) {
-  if (context.params.query) {
-    delete context.params.query.include;
-  }
-  return context;
-};
-
-
-/*
- * Default new instances into the latest update record in the `general` channel.
- */
-module.exports.defaultUpdateLevel = async function(context) {
-  const updates = context.app.service('update');
-  const result = await updates.find({
-    query: {
-      tainted: false,
-      channel: 'general',
-      $limit: 1,
-      $sort: {
-        createdAt: -1,
-      }
-    },
-  });
-
-  if (result.total == 0) {
-    throw new Error('Failed to find the latest `general` updates for instance creation');
-  }
-  /*
-    * The result returned is a paginated object
-    */
-  context.data.updateId = result.data[0].id;
-  return context;
-};
-
-
-/*
- * mash everything into module.exports for easy of use in the service
- * definition
- */
-Object.assign(module.exports, {
-  before: {
-    all: [
-      authentication.hooks.authenticate(['jwt'])
-    ],
-    find: [
-    ],
-    get: [
-      module.exports.includeAssociations,
-    ],
-
-    create: [
-      ensureMatchingUUID,
-      module.exports.defaultUpdateLevel,
-      module.exports.pruneQueryParams,
-    ],
-
-    update: [
-      ensureMatchingUUID,
-    ],
-    patch: [
-      ensureMatchingUUID,
-    ],
-    remove: [
-      internalOnly
-    ]
-  },
-
-  after: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  },
-
-  error: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
-  }
-});
+module.exports = new StatusHooks();
