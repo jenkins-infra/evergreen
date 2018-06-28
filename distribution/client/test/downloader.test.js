c2a574872e292bb09f6f4f1d9c2947353dab6909
Baptiste Mathus <batmat@batmat.net>
2018-06-28 12:17:34 +0200

    [JENKINS-52234] Make sure directory exists

--- distribution/client/test/downloader.test.js
+++ distribution/client/test/downloader.test.js
@@ -1,11 +1,16 @@
 'use strict';
 
 const Downloader = require('../src/lib/downloader');
+const mkdirp     = require('mkdirp');
 
 describe('the Downloader class', () => {
   describe('download()', () => {
     let item = 'https://jenkins.io';
     let dir  = '/tmp/jest';
+
+    beforeEach(() => {
+      mkdirp.sync(dir);
+    });
 
     it('should return  promise', () => {
       let response = Downloader.download(item, dir);
