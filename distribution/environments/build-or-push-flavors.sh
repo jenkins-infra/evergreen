cd36b6a1b11cdf7b98cbccad00fc3ba91fdaab22
Baptiste Mathus <batmat@batmat.net>
2018-07-02 10:00:08 +0200

    Make building many flavors less copy-paste
    Maybe in the future, we'll even want to have some kind of templatized
    Dockerfile.
    I felt this is not necessary for now though I considered doing it.
    Let's rather have it all working first :).

---
+++ distribution/environments/build-or-push-flavors.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -euo pipefail
+
+IMAGE_NAME=batmat/evergreen
+
+# docker build -t $IMAGE_NAME:$currentDir .
+# docker push $IMAGE_NAME:$currentDir
+
+if [[ "$1" == "build" ]]; then
+  for dir in *-cloud
+  do
+    echo "Building $dir directory"
+    docker build -t "$IMAGE_NAME:$dir" "$dir"
+  done
+elif [[ "$1" == "push" ]]; then
+  for dir in *-cloud
+  do
+    echo "Push $dir image"
+    docker push "$IMAGE_NAME:$dir"
+  done
+else
+  echo "Unknown parameter, failing."
+  exit 1
+fi
