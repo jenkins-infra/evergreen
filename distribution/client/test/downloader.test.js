4a8bb1603515993eee9a717fefb92207019cf53d
Baptiste Mathus <batmat@batmat.net>
2018-07-17 07:44:53 +0200

    Add bigger file download and checksum check

--- distribution/client/test/downloader.test.js
+++ distribution/client/test/downloader.test.js
@@ -2,6 +2,8 @@ jest.mock('fs');
 
 const Downloader = require('../src/lib/downloader');
 const mkdirp     = require('mkdirp');
+const fs         = require('fs');
+const checksum   = require('checksum');
 
 describe('the Downloader class', () => {
   describe('download()', () => {
@@ -12,9 +14,12 @@ describe('the Downloader class', () => {
       mkdirp.sync(dir);
     });
 
-    it('should return promise', () => {
+    it('should return promise', async () => {
       let response = Downloader.download(`${item}/index.html`, dir);
-      expect(Promise.resolve(response)).toBe(response);
+      expect(response instanceof Promise).toBeTruthy();
+      await response;
+
+      expect(fs.readFileSync(`${dir}/index.html`)).toBeTruthy();
     });
 
     it('should fail on url without final basename-ish path', () => {
@@ -22,6 +27,19 @@ describe('the Downloader class', () => {
         Promise.resolve(Downloader.download(`${item}/`, dir));
       }
       ).toThrow();
+    });
+
+    it('should manage to download a decently big file, retrying if needed', async () => {
+
+      // ace-editor is 5 MB, so it could make tests more flaky and cumbersome with slow connection
+      // FIXME: introduce assume() + env var to allow disabling this?
+      const toDownload = 'http://updates.jenkins-ci.org/download/plugins/ace-editor/1.1/ace-editor.hpi';
+      const expectedSha1 = '39ed0947dcd9b414769ae28b3eb955643e25d5e0';
+
+      await Downloader.download(toDownload, dir);
+
+      expect(checksum(fs.readFileSync(`${dir}/ace-editor.hpi`))).toBe(expectedSha1);
+
     });
   });
 });
