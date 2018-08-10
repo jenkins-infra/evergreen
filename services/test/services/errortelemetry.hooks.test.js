363bf82a13aff183f89938c5ed26b101f6bcd030
Mandie Smith <asmith@cloudbees.com>
2018-08-10 16:56:27 -0400

    JENKINS-52904 - update error telemetry tests to use expect and test positive and negative cases

--- services/test/services/errortelemetry.hooks.test.js
+++ services/test/services/errortelemetry.hooks.test.js
@@ -1,30 +1,46 @@
-const assert         = require('assert');
 const errors         = require('@feathersjs/errors');
 
 const checkLogFormat = require('../../src/services/errorTelemetry/errorTelemetry.hooks').checkLogFormat;
 
 describe('Error Telemetry Hooks', () => {
   it('should fail with empty parameters', () => {
-    try {
+    expect(() => {
       checkLogFormat();
-    } catch (err) {
-      assert.ok( err instanceof errors.BadRequest );
-    }
+    }).toThrow(/No hook at all/);
+  });
+
+  it('missing data should be rejected', async() => {
+    expect(() => {
+      checkLogFormat({});
+    }).toThrow(/Missing data/);
+  });
+
+  it('missing log field should be rejected', async() => {
+    expect(() => {
+      checkLogFormat({data: {}});
+    }).toThrow(/Missing log field/);
   });
 
   it('missing fields should be rejected', async () => {
     const badQueries = [
-      {},
-      {'log':{'version': 1 }},
-      {'log':{'timestamp': 1526387942 }}
+      {data: {'log':{}}},
+      {data: {'log':{'version': 1 }}},
+      {data: {'log':{'version': 1, 'timestamp': 1526387942 }}},
+      {data: {'log':{'version': 1, 'timestamp': 1526387942 }}},
+      {data: {'log':{'version': 1, 'timestamp': 1526387942 }, 'name': 'name'}},
+      {data: {'log':{'version': 1, 'timestamp': 1526387942 }, 'name': 'name', 'level': 'info'}},
     ];
     for (let i = 0; i < badQueries.length; i++) {
-      try {
+      expect(() => {
         checkLogFormat(badQueries[i]);
-        assert.fail('Should have failed above (value=${badQueries[i]})');
-      } catch (err) {
-        assert.ok( err instanceof errors.BadRequest );
-      }
+      }).toThrow(/Missing required field/);
     }
+  });
+
+  it('should pass with valid data', async() => {
+    let hook = { data : {'log':{'timestamp': 1526387942, 'version': 1, 'name': 'name', 'level': 'info', 'message': 'message' }} };
+    expect(() => {
+      checkLogFormat(hook);
+    }).not.toThrow(errors.BadRequest);
   });
 });
