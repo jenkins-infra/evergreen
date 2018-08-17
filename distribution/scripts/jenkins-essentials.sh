9f0d1bc74b000ec27d0639f6d79c622150d9026d
Baptiste Mathus <batmat@batmat.net>
2018-08-17 15:48:24 +0200

    [JENKINS-53059] Fixing the unbound variable issue

--- distribution/scripts/jenkins-essentials.sh
+++ distribution/scripts/jenkins-essentials.sh
@@ -16,7 +16,7 @@ echo "Done. Password value stored in $passwordFileLocation file."
 JENKINS_ADMIN_PASSWORD="$( cat "$passwordFileLocation" )"
 
 # Intended for ease of development. By default, password is obviously *not* put in logs.
-if [[ -n $INSECURE_SHOW_ADMIN_PASSWORD ]]; then
+if [[ "${INSECURE_SHOW_ADMIN_PASSWORD:-yes}" == "true" ]]; then
   echo "[WARNING] INSECURE_SHOW_ADMIN_PASSWORD defined, it should only ever be done for testing."
   echo "[admin password] $JENKINS_ADMIN_PASSWORD"
 fi
