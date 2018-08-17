e883c5514e2d46c843fc03cbf89e74ece8f33604
Baptiste Mathus <batmat@batmat.net>
2018-08-17 15:43:45 +0200

    [JENKINS-53059] Jenkins fails to start: INSECURE_SHOW_ADMIN_PASSWORD: unbound variable
    Test showing this fails. (I.e. the fix is *not* here, will be in next commit)

--- distribution/tests/offline-tests.sh
+++ distribution/tests/offline-tests.sh
@@ -68,4 +68,18 @@ test_jep_307() {
   assertEquals "everything else should not validate" "60" "$?"
 }
 
+# JENKINS-53059
+test_INSECURE_SHOW_ADMIN_PASSWORD_can_be_unset() {
+  $result=( docker run jenkins/evergreen:$ENVIRONMENT jenkins-essentials.sh )
+  # Expected to fail, but because war is missing. Admin password must have been generated,
+  # and final expected error line is "Error: Unable to access jarfile /evergreen/jenkins/home/jenkins.war"
+  assertNotEquals "Should have failed to start up (war is absent)" "0" "$?"
+
+  cat "$result" | grep '[admin password] '
+  assertEquals "Line with generated password should have been found" "0" "$?"
+
+  cat "$result" | tail -1 | grep 'Error: Unable to access jarfile /evergreen/jenkins/home/jenkins.war'
+  assertEquals "Last line should be about the missing jenkins.war" "0" "$?"
+}
+
 . ./shunit2/shunit2
