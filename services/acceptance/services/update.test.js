6732d7975497c091f4e90ce67cd835d0415e198f
R. Tyler Croy <tyler@monkeypox.org>
2018-08-23 16:58:23 -0700

    Add an acceptance tests for the socket.io-based event system
    This has certainly allowed me to attach a debugger into the
    app.service().publish() hooks in channel.js for @mandie722

--- services/acceptance/services/update.test.js
+++ services/acceptance/services/update.test.js
@@ -9,6 +9,14 @@ const assert   = require('assert');
 const request  = require('request-promise');
 const h        = require('../helpers');
 
+/*
+ * Necessary for testing events sent to a socket.io client when updates are
+ * created
+ */
+const feathers = require('@feathersjs/feathers');
+const socketio = require('@feathersjs/socketio-client');
+const io       = require('socket.io-client');
+
 describe('Update service acceptance tests', () => {
   beforeAll((done) => {
     this.ingest = JSON.parse(fs.readFileSync('./ingest.json'));
@@ -95,6 +103,43 @@ describe('Update service acceptance tests', () => {
           expect(res.body.id).toBeGreaterThan(0);
         })
         .catch((err) => assert.fail(err));
+    });
+
+    describe('with a socket.io client connected', () => {
+      beforeEach(() => {
+        this.socket = io(h.getUrl(''));
+        this.client = feathers();
+        this.client.configure(socketio(this.socket));
+      });
+
+      afterEach(() => {
+        this.socket.close();
+      });
+
+      it('should emit an event when an update is created', async () => {
+        let found = false;
+
+        this.client.service('update').on('created', (message) => {
+          expect(message).toBeTruthy();
+          found = true;
+        });
+
+        const response = await request({
+          url: h.getUrl('/update'),
+          method: 'POST',
+          headers: { 'Authorization': this.settings.internalAPI.secret },
+          json: true,
+          resolveWithFullResponse: true,
+          body: {
+            commit: Date.now().toString(),
+            manifest: this.ingest,
+          },
+        });
+
+        expect(response.statusCode).toEqual(201);
+        expect(found).toBeTruthy();
+        expect.assertions(3);
+      });
     });
   });
 
