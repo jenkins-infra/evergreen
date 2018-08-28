d9a860b55c77439455561d4aea4517d8a9eae43d
R. Tyler Croy <tyler@monkeypox.org>
2018-08-28 09:36:35 -0700

    Refactor some common node GNu/Make tooling up into the root directory

---
+++ node.mk
@@ -0,0 +1,37 @@
+# Clever: https://stackoverflow.com/a/324782
+NODE:=$(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))/tools/node
+
+all: check
+
+lint: depends
+	$(NODE) npm run eslint
+
+fix-formatting: depends
+	$(NODE) npm run eslint -- --fix
+
+check: lint
+	$(MAKE) unit
+
+unit: depends
+	if [ -z "$${SKIP_TESTS}" ]; then \
+		$(NODE) npm run jest; \
+	else \
+		echo "Tests are skipped!"; \
+	fi;
+
+debug-unit: depends
+	$(NODE) node --inspect-brk=0.0.0.0:9229 node_modules/.bin/jest --runInBand --bail --forceExit test/
+
+depends: node_modules
+
+node_modules: package-lock.json package.json
+	# Checking to see if the directory exists because npm install updates the
+	# directory every time it runs, busting the GNU/Make cache causing rebuilds
+	if [ ! -d node_modules ]; then \
+		$(NODE) npm install; \
+	fi;
+
+clean:
+	rm -rf vendor node_modules build
+
+.PHONY: all check clean depends run unit lint
