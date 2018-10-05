36bf05f96d2926e66d347ea44e100cae7ae54c6a
Baptiste Mathus <batmat@batmat.net>
2018-10-05 18:56:22 +0200

    Move the whole waiting block in dev mode only
    In non-dev mode, anyway, Supervisord would just restart the client failing
    to start.
    
    Here in dev mode, the difference is we explicitly want the client to
    *wait* for the backend to be ready.

--- distribution/scripts/start-client.sh
+++ distribution/scripts/start-client.sh
@@ -4,24 +4,24 @@
 # Should only be useful during development
 set -euo pipefail
 
-sleepTime=8
 if [ ! -z "${DEVELOPMENT:-}" ]; then
+  sleepTime=8
   echo "DEVELOPMENT MODE: client will wait $sleepTime before starting, to give time to the backend to start and receive a first UL"
   sleep $sleepTime
+
+  maxAttempts=30
+  until curl -s "$EVERGREEN_ENDPOINT" --output /dev/null ; do
+    maxAttempts=$(( maxAttempts - 1 ))
+    if [[ $maxAttempts -le 0 ]]; then
+      >&2 echo "Maximum number of attempts reached: exiting"
+      exit 1
+    fi
+    >&2 echo "Backend is unavailable - sleeping for some more time"
+    sleep 1
+  done
+
 else
   echo "Client is starting up"
 fi
-
-maxAttempts=30
-
-until curl -s "$EVERGREEN_ENDPOINT" --output /dev/null ; do
-  maxAttempts=$(( maxAttempts - 1 ))
-  if [[ $maxAttempts -le 0 ]]; then
-    >&2 echo "Maximum number of attempts reached: exiting"
-    exit 1
-  fi
-  >&2 echo "Backend is unavailable - sleeping for some more time"
-  sleep 1
-done
 
 exec /usr/local/bin/npm run client
