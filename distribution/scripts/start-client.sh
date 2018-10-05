0853e43d5937727a0e1b54ff89bc4e36640c9707
Baptiste Mathus <batmat@batmat.net>
2018-10-05 17:07:03 +0200

    Enable shellcheck on new script and fix issues

--- distribution/scripts/start-client.sh
+++ distribution/scripts/start-client.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 # Wrapper to force the client to wait for the backend to become available
 # Should only be useful during development
 set -euo pipefail
 
 sleepTime=8
-if [ ! -z ${DEVELOPMENT:-} ]; then
+if [ ! -z "${DEVELOPMENT:-}" ]; then
   echo "DEVELOPMENT MODE: client will wait $sleepTime before starting, to give time to the backend to start and receive a first UL"
   sleep $sleepTime
 else
@@ -14,9 +14,9 @@ fi
 
 maxAttempts=30
 
-until curl -s $EVERGREEN_ENDPOINT --output /dev/null ; do
-  maxAttempts=$(( $maxAttempts - 1 ))
-  if [[ $maxAttempts <= 0 ]]; then
+until curl -s "$EVERGREEN_ENDPOINT" --output /dev/null ; do
+  maxAttempts=$(( maxAttempts - 1 ))
+  if [[ $maxAttempts -le 0 ]]; then
     >&2 echo "Maximum number of attempts reached: exiting"
     exit 1
   fi
