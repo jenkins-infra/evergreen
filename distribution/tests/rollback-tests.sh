90fdca47eace25e550f7706616a40659de9f1f73
Baptiste Mathus <batmat@batmat.net>
2018-10-15 13:21:41 +0200

    Moar linting - shellcheck & jsonlint here

--- distribution/tests/rollback-tests.sh
+++ distribution/tests/rollback-tests.sh
@@ -7,8 +7,6 @@ export PATH="$current_directory/../../tools:$PATH"
 
 echo "Debugging: PATH=***$PATH***"
 
-JENKINS_HOME=to_override
-
 # shellcheck source=tests/utilities
 . "$current_directory/utilities"
 
@@ -20,20 +18,19 @@ oneTimeSetUp() {
   upload_update_level "./tests/rollback/1-ingest-ok.json"
 
   wait_for_jenkins
-  # shellcheck disable=SC2016
-  JENKINS_HOME="$( docker exec "$container_under_test" bash -c 'echo $JENKINS_HOME' )"
 }
 
 test_rollback() {
 
   # Check UL is the correct one (UL 1 or UL 2?!)
+  # shellcheck disable=SC2016
   docker exec "$container_under_test" bash -c 'ls $EVERGREEN_DATA'
   # extract 2 from `"level":2` from the updates.json file
+  # shellcheck disable=SC2016
   correctUL=$( docker exec "$container_under_test" bash -c 'cat $EVERGREEN_DATA/updates.json' | \
                                     grep --only-matching '"level":.' | \
                                     cut -d : -f 2  )
   assertEquals "Command should have succeeded" 0 "$?"
-
   assertEquals "Should be UL 2" 2 "$correctUL"
 
   # upload borked update level to backend
@@ -47,7 +44,9 @@ test_rollback() {
 
   # let's now check the upload and upgrade attempt to borked UL3 *actually* happened
   # because if this didn't, then we'd still on UL2, but not testing there was a rollback somewhere
+  # shellcheck disable=SC2016
   beforeLastUpdate=$( docker exec "$container_under_test" bash -c 'cat $EVERGREEN_DATA/updates.auditlog' | tail -2 | head -1 | jq -r .updateLevel )
+  # shellcheck disable=SC2016
   lastUpdate=$( docker exec "$container_under_test" bash -c 'cat $EVERGREEN_DATA/updates.auditlog' | tail -1 | jq -r .updateLevel )
 
   assertEquals "Previous UL should be 3, the one expected to be rolled back" 3 "$beforeLastUpdate"
